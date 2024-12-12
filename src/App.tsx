e07b69a8466bd082d059ce22f5fff62516f14635
syedahmedhussain <ShahSahabTheDeveloper@gmail.com>
2024-12-11 21:44:54 -0600

    Variant2: Another  Image Scale in / Scale out based on window.size

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { FC } from "react";
 import { useEffect, useState } from "react";
 import MySvg from "./assets/hollowed-boxes.svg";
+import "./App.css";
 
 const App: FC = () => {
   const [scale, setScale] = useState(1);
+  // const [prevSize, setPrevSize] = useState(1);
+  // const imgRef = React.createRef<HTMLImageElement>();
 
   useEffect(() => {
     const handleResize = () => {
       const newScale = window.innerWidth / 1000;
       setScale(newScale);
     };
+    // const handleResize = () => {
+    //   const newSize = window.innerWidth / 1000;
+    //   if (newSize !== prevSize) {
+    //     setPrevSize(newSize);
+    //     imgRef.current && imgRef.current.style.setProperty("--scale", newSize.toString());
+    //   }
+    // };
 
     window.addEventListener("resize", handleResize);
     handleResize();
@@ -20,7 +30,7 @@ const App: FC = () => {
   }, []);
 
   return (
-    <div>
+    <div className="App">
       <h1>Hello, React 18!</h1>
       <img src={MySvg} alt="My SVG" style={{ transform: `scale(${scale})` }} />
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { ImageResize } from "./image-resize/ImageResize";
 
 // Render your React component instead
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <>
+      <App />
+      <ImageResize />
+    </>
   </StrictMode>
 );
